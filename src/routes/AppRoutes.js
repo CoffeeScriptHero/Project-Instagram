0479afe078cf3ed14ee40348f721006bbeda6532
CoffeeScriptHero <denis_kozarenko@ukr.net>
2021-11-12 19:44:18 +0200

    comment updated + like

--- src/routes/AppRoutes.js
+++ src/routes/AppRoutes.js
@@ -1,19 +1,19 @@
 import React from "react";
-import {Route, Switch} from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import Main from "../pages/Main/Main";
 import User from "../pages/User/User";
 
 const AppRoutes = () => {
-    return (
-        <Switch>
-            <Route exact path="/Main">
-                <Main/>
-            </Route>
-            <Route exact path="/User">
-                <User nickname="Sladkiy_bubalekh" description="Hello everyone"/>
-            </Route>
-        </Switch>
-    );
+  return (
+    <Switch>
+      <Route exact path="/Main">
+        <Main />
+      </Route>
+      <Route exact path="/User">
+        <User nickname="Sladkiy_bubalekh" description="Hello everyone" />
+      </Route>
+    </Switch>
+  );
 };
 
 export default AppRoutes;
