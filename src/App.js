332a5e5c6778911315b8ec44453e7d9f1d0cb4eb
CoffeeScriptHero <denis_kozarenko@ukr.net>
2021-11-14 21:03:41 +0200

    comment form fixes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import "./App-styles.scss";
 import AppRoutes from "./routes/AppRoutes.js";
 import Header from "./components/Header/Header.js";
-import PostModal from "./components/PostModal/PostModal";
-import img from "./images/waterfall.jpg";
-import user from "./images/user.jpg";
+// import PostModal from "./components/PostModal/PostModal";
+// import img from "./images/waterfall.jpg";
+// import user from "./images/user.jpg";
 const App = () => {
   return (
     <div className="app-wrapper">
       <Header />
-      <PostModal
+      {/* <PostModal
         img={img}
         userImg={user}
         nickname={"deniskozarenko"}
@@ -17,7 +17,7 @@ const App = () => {
           { nickname: "igor", text: "great" },
           { nickname: "vasya", text: "1212?" },
         ]}
-      />
+      /> */}
       <AppRoutes />
     </div>
   );
